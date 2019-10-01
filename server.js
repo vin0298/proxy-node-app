1818ced55a218a3142403bfe5cee3488db30a547
Vincent Surya Bensawan <bensawan0298@gmail.com>
2019-10-01 01:01:32 -0700

    Showed the bytes and corresponding URL

--- server.js
+++ server.js
@@ -4,21 +4,23 @@ const app = express();
 const router = express.Router();
 const request = require('request');
 const bodyParser = require('body-parser');
+const path = require('path');
 
+// Store the response body of cached pages
 var cached_pages = {};
+// Store the bytesRead of the response body stored
 var cached_bytes = {};
 
 app.set('view engine', 'pug')
 
 app.get('/', (req, res)=> {
-    res.render('index')
+    res.render('home')
 })
 
-app.listen(3000)
+app.listen(8000)
 
 app.use(bodyParser.urlencoded({extended: true}))
 
-// Add validation
 app.post('/submit', (req, res) => {
     var targetURL = req.body.targeturl;
     if (targetURL in cached_pages) {
@@ -29,7 +31,9 @@ app.post('/submit', (req, res) => {
          * TODO: Update cached page (if there are any updates)
          *       Show byte sizes
          *       Simple form validation
-         *       
+         * Current Limitations: Can't download assets (External css)
+         *                      Dynamic pages
+         *                      Strictly one link at a time
          */
         request(targetURL, function(error, response, body) {
             if (!error && parseInt(response.statusCode) == 200) { 
@@ -43,4 +47,13 @@ app.post('/submit', (req, res) => {
     } 
 })
 
+app.get('/index', (req, res) => {
+    res.render('test', {cached_pages: cached_pages, cached_bytes: cached_bytes});
+})
+
 // Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36
+// var options = {
+//     url: page,
+//     headers: {
+//       'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36'
+//     }
