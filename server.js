c40dfa9f6ad13afde2a54e3f344c93a20a4fb8ed
Vincent Surya Bensawan <bensawan0298@gmail.com>
2019-09-30 23:09:41 -0700

    Implemented a minimal caching feature

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const router = express.Router();
+const request = require('request');
+const bodyParser = require('body-parser');
+
+var cached_pages = {};
 
 app.set('view engine', 'pug')
 
@@ -11,6 +15,20 @@ app.get('/', (req, res)=> {
 })
 
 app.listen(3000)
-// router.get('/',function(req,res){
-//     res.sendFile(path.join(__dirname+'/index.html'));
-//   });
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+// Add validation
+app.post('/submit', (req, res) => {
+    var targetURL = req.body.targeturl;
+    console.log("target url: " + targetURL);
+    if (targetURL in cached_pages) {
+        console.log("sending from memory");
+        res.send(cached_pages[targetURL]);
+    } else {
+        request(targetURL, function(error, response, body) {
+            cached_pages[targetURL] = body;
+            res.send(body);
+        })
+    } 
+})
