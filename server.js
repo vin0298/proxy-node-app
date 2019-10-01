31832f0066a864256e343db75cc2308ec951ce24
Vincent Surya Bensawan <bensawan0298@gmail.com>
2019-10-01 02:09:27 -0700

    Used redis for caching instead of normal map

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const request = require('request');
 const bodyParser = require('body-parser');
 const path = require('path');
 
+// Initialised redis to cache response body
+const redis = require('redis'),
+    client = redis.createClient();
+client.on('connect', ()=> console.log('Redis connected'))
+
 // Store the response body of cached pages
 var cached_pages = {};
 // Store the bytesRead of the response body stored
@@ -21,35 +26,48 @@ app.listen(8000)
 
 app.use(bodyParser.urlencoded({extended: true}))
 
+
+/**
+ * TODO: Update cached page (if there are any updates) : etags (?), last-modified
+ *       Show byte sizes
+ *       Simple form validation
+ * Current Limitations: Can't download assets (External css)
+ *                      Dynamic pages
+ *                      Strictly one link at a time
+ *                      Some pages encounter "Update browser problem" : facebook
+ *                      Redis not exited cleanly
+ */
+
 app.post('/submit', (req, res) => {
     var targetURL = req.body.targeturl;
-    if (targetURL in cached_pages) {
-        console.log("Sending from memory....");
-        res.send(cached_pages[targetURL]);
-    } else {
-        /**
-         * TODO: Update cached page (if there are any updates)
-         *       Show byte sizes
-         *       Simple form validation
-         * Current Limitations: Can't download assets (External css)
-         *                      Dynamic pages
-         *                      Strictly one link at a time
-         */
-        request(targetURL, function(error, response, body) {
-            if (!error && parseInt(response.statusCode) == 200) { 
-                cached_pages[targetURL] = body;
-                cached_bytes[targetURL] = response.socket.bytesRead;
-                res.send(body);
-            } else {
-                console.log("invalid url: " + error);
-            }
-        })
-    } 
+
+    client.get(targetURL, (err, result) => {
+        if (err == null && result != null) {
+            console.log("sending from memory");
+            res.send(result);
+        } else {
+            request(targetURL, function(error, response, body) {
+                if (!error && parseInt(response.statusCode) == 200) { 
+                    client.set(targetURL, body, (err, reply) => {
+                        if (reply == 'OK') {
+                            res.send(body);
+                        }
+                    })
+                    cached_pages[targetURL] = body;
+                    cached_bytes[targetURL] = response.socket.bytesRead;
+                } else {
+                    console.log("invalid url: " + error);
+                }
+            })
+        }
+    })
 })
 
 app.get('/index', (req, res) => {
     res.render('test', {cached_pages: cached_pages, cached_bytes: cached_bytes});
 })
+
+
 
 // Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36
 // var options = {
