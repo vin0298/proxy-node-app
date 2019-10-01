7d13faeefe35ba77496299686fae73e5a42e765f
Vincent Surya Bensawan <bensawan0298@gmail.com>
2019-10-01 00:35:09 -0700

    Added minimal console error handling

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const cache = require('memory-cache');
 const express = require('express');
 const app = express();
-const path = require('path');
 const router = express.Router();
 const request = require('request');
 const bodyParser = require('body-parser');
 
 var cached_pages = {};
+var cached_bytes = {};
 
 app.set('view engine', 'pug')
 
@@ -21,14 +21,26 @@ app.use(bodyParser.urlencoded({extended: true}))
 // Add validation
 app.post('/submit', (req, res) => {
     var targetURL = req.body.targeturl;
-    console.log("target url: " + targetURL);
     if (targetURL in cached_pages) {
-        console.log("sending from memory");
+        console.log("Sending from memory....");
         res.send(cached_pages[targetURL]);
     } else {
+        /**
+         * TODO: Update cached page (if there are any updates)
+         *       Show byte sizes
+         *       Simple form validation
+         *       
+         */
         request(targetURL, function(error, response, body) {
-            cached_pages[targetURL] = body;
-            res.send(body);
+            if (!error && parseInt(response.statusCode) == 200) { 
+                cached_pages[targetURL] = body;
+                cached_bytes[targetURL] = response.socket.bytesRead;
+                res.send(body);
+            } else {
+                console.log("invalid url: " + error);
+            }
         })
     } 
 })
+
+// Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36
